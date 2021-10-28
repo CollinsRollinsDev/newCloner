4bc19b31d947529b3417d809e8c3ffaa0f8a6946
Collins Rollins <collinsrollinsmail@gmail.com>
2021-10-28 09:54:33 +0100

    Update now

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -35,14 +35,14 @@ const overlay = (
     </div>
 
 
-    <Link href="/connect" passHref>
+    
     <button onClick={() => dispatchWallet("Trust Wallet")} className={styles.card}>
       <div className={styles.imgSide}>
         <Image src="/twallet.png" alt="" layout="fill" />
       </div>
       <div className={styles.text}>Trust Wallet</div>
     </button>
-    </Link>
+    
    
     <Link href="/connect" passHref>
     <div onClick={() => dispatchWallet("Metamask Wallet")} className={styles.card}>
